8e3e6dd89697f204be64b5eb5617c5bbbb9af75e
ashwin925 <ashwinsundar90250@gmail.com>
2024-10-14 20:33:58 +0530

    contact-form

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
-import Templates from './components/templates';
+// import Templates from './components/templates';
 // import Homepage from './components/home'; 
 //  import About from './components/about'; 
-// import Projects from './components/projects';    
+// import Projects from './components/projects';   
+import Contact from './components/contact'; 
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Templates />} />
+        <Route path="/" element={<Contact />} />
+        {/* <Route path="/" element={<Templates />} /> */}
         {/* <Route path="/" element={<About />} /> */}
         {/* <Route path="/" element={<Projects />} /> */}
         {/* <Route path="/" element={<Homepage />} /> */}
--- src/components/contact.js
+++ src/components/contact.js
@@ -1,1 +1,61 @@
-//contains contact me form
+import React from 'react';
+import './contact.css';
+
+export default function Contact() {
+  return (
+    <div className='templates-container'>
+      <div className="ripples-container"></div>
+      <form className="contact-form">
+      <h1 className="contact-title font-semibold">Contact Me</h1>
+        {/* Name */}
+        <div className="mb-4">
+          <label className=" tc block text-gray-700 font-bold mb-2" htmlFor="name">
+            Name
+          </label>
+          <input
+            className="input-field w-[240px]"
+            id="name"
+            type="text"
+            placeholder="Enter your name"
+          />
+        </div>
+
+        {/* Gmail */}
+        <div className="mb-4">
+          <label className="tc block text-gray-700 font-bold mb-2" htmlFor="email">
+            Gmail
+          </label>
+          <input
+            className="input-field w-[240px]"
+            id="email"
+            type="email"
+            placeholder="Enter your Gmail"
+          />
+        </div>
+
+        {/* Message */}
+        <div className="mb-6">
+          <label className="tc block text-gray-700 font-bold mb-2" htmlFor="message">
+            Message
+          </label>
+          <textarea
+            className="input-field h-[100px] w-[240px]"
+            id="message"
+            rows="5"
+            placeholder="Enter your message"
+          ></textarea>
+        </div>
+
+        {/* Submit Button */}
+        <div className="flex items-center justify-between">
+          <button
+            className="submit-button"
+            type="submit"
+          >
+            Submit
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+}
